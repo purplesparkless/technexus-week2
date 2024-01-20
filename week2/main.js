46a342bec43489c87c6f909b415041d8abf53453
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-20 19:08:00 +0100

    weekTwo commit

--- week2/main.js
+++ week2/main.js
@@ -1,11 +1,21 @@
-let a = 10;
-let b = 5;
-console.log(a + b);
-console.log(a - b);
-console.log ( a**b);
-let c = 20;
-c+=b;
-a-=c;
-let d = a%c;
-let e = a **d;
+// let a = 10;
+// let b = 5;
+// console.log(a + b);
+// console.log(a - b);
+// console.log ( a**b);
+// let c = 20;
+// c+=b;
+// a-=c;
+// let d = a%c;
+// let e = a **d;
 
+// Excercise
+let a = 30;
+
+if (a%3 == 0 && a%5 == 0){
+    console.log("FIZZ");
+}else if(a%5 == 0){
+    console.log("BUZZ")
+}else{
+    console.log(a)
+}
